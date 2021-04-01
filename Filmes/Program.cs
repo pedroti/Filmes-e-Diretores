08c23a3ca6160eeb97dbba3b7c32907dca9d96f2
pedrotiz <phtizatto@ucs.br>
2021-03-31 22:43:43 -0300

    Adicionar arquivos de projeto.

---
+++ Filmes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Filmes
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            string directorsPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName, "directors.txt");
+            ArrayList diretores = new ArrayList();
+            try
+            {
+                using (StreamReader sr = new StreamReader(directorsPath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.Out.WriteLine(line);
+                        diretores.Add(line.Split(','));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
